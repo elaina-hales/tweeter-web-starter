7d01deb1f9844f34a6da03de3371fdaa904d24ab
Elaina Hales <evhales@byu.edu>
2025-11-11 12:01:42 -0700

    making loadMoreFollowees Lambda, API calls, client adjustments

--- tweeter-web/src/model.service/FollowService.ts
+++ tweeter-web/src/model.service/FollowService.ts
@@ -1,25 +1,32 @@
 import { AuthToken, User, FakeData } from "tweeter-shared";
 import { Service } from "./Service";
+import { ServerFacade } from "../network/ServerFacade";
 
 export class FollowService implements Service {
+  private serverFacade = new ServerFacade;
+
     public async loadMoreFollowees (
-        authToken: AuthToken,
-        userAlias: string,
-        pageSize: number,
-        lastItem: User | null
-      ): Promise<[User[], boolean]> {
-          // TODO: Replace with the result of calling server
-        return FakeData.instance.getPageOfUsers(lastItem, pageSize, userAlias);
+      authToken: AuthToken,
+      userAlias: string,
+      pageSize: number,
+      lastItem: User | null
+    ): Promise<[User[], boolean]> {
+      // TODO: Replace with the result of calling server
+      if (lastItem === null) {
+        return this.serverFacade.getMoreFollowees({token: authToken.token, userAlias: userAlias, pageSize: pageSize, lastItem: null});
+      } else {
+        return this.serverFacade.getMoreFollowees({token: authToken.token, userAlias: userAlias, pageSize: pageSize, lastItem: lastItem.dto});
+      }
     };
     
     public async loadMoreFollowers (
-        authToken: AuthToken,
-        userAlias: string,
-        pageSize: number,
-        lastItem: User | null
-      ): Promise<[User[], boolean]> {
-        // TODO: Replace with the result of calling server
-        return FakeData.instance.getPageOfUsers(lastItem, pageSize, userAlias);
+      authToken: AuthToken,
+      userAlias: string,
+      pageSize: number,
+      lastItem: User | null
+    ): Promise<[User[], boolean]> {
+      // TODO: Replace with the result of calling server
+      return FakeData.instance.getPageOfUsers(lastItem, pageSize, userAlias);
     };
 
 }
